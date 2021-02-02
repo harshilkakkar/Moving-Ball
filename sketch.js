92869060a77bb34a5b3a4c892d65135bddbfb889
Harshil Kakkar <kakkar.harshil@gmail.com>
2021-02-02 20:53:32 +1100

    Today we learned about real-time databases

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
-var ball;
+var hypnoticBall, Position, database;
 
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+    
+    var hypnoticBallPosition = database.ref('Ball/Position');
+    hypnoticBallPosition.on("value", readPosition, showError);
+
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(Position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
+    
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('Ball/Position').set({
+        "x": Position.x + x,
+        "y": Position.y + y
+    })
+}
+
+function showError(){
+    console.log("Error in reading the database");
+}
+
+function readPosition(data){
+Position = data.val();
+console.log(Position.x);
+hypnoticBall.x = Position.x;
+hypnoticBall.y = Position.y;
 }
